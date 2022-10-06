c2162e85243927f317e4dfadebb4c70efbea16b3
77251688 <907624307@qq.com>
2022-10-06 23:23:53 +0800

    更改返回值

--- src/main/java/com/cummins/pluginshop/service/upload/Upload.java
+++ src/main/java/com/cummins/pluginshop/service/upload/Upload.java
@@ -1,9 +1,11 @@
 package com.cummins.pluginshop.service.upload;
 
+import com.cummins.pluginshop.entity.Resp;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 public interface Upload {
-    void saveFile(MultipartFile file) throws URISyntaxException;
+    <T> Resp<T> saveFile(MultipartFile file) throws URISyntaxException, IOException;
 }
