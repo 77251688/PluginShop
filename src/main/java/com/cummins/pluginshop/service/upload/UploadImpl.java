2c7679d8a3793301fdafa8322c8dc5ff53b6a58e
77251688 <907624307@qq.com>
2022-10-06 23:25:00 +0800

    文件上传完成

--- src/main/java/com/cummins/pluginshop/service/upload/UploadImpl.java
+++ src/main/java/com/cummins/pluginshop/service/upload/UploadImpl.java
@@ -1,18 +1,31 @@
 package com.cummins.pluginshop.service.upload;
 
+import com.cummins.pluginshop.entity.Resp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 @Service
 @Slf4j
 public class UploadImpl implements Upload {
     @Override
-    public void saveFile(MultipartFile file) throws URISyntaxException {
-        String filename = file.getOriginalFilename();
-        System.out.println(this.getClass().getResource("/").toURI());
-        log.warn(filename);
+    public Resp<String> saveFile(MultipartFile file) {
+        String filename = file.getOriginalFilename().substring(0, file.getOriginalFilename().lastIndexOf("."));
+        String fileOriginalName = file.getOriginalFilename();
+        File file1 = new File(this.getClass().getResource("/").getPath() + filename);
+        if (!file1.exists()) {
+            file1.mkdir();
+        }
+        try {
+            file.transferTo(new File(file1 + "\\" + fileOriginalName));
+            return Resp.success("上传成功", "path" + file1 + "\\" + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Resp.failed("500", "", "null");
+        }
     }
 }
